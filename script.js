e3b92a6e78bba8ad1577894b4d07455ce89677e1
Ruben Sanchez Malca <68285220+rbn-malca@users.noreply.github.com>
2024-10-08 11:38:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 const products = [
    {
-    name: "QR Pro Scanner",
-    price: 409.99,
-    description: "Escáner de código de barras de escritorio, código QR, certificado Digital europeo, 2D",
-    image: "https://example.com/image_of_qr_pro_scanner.png",
-    video: "https://www.youtube.com/watch?v=xs-8jOXLKqE"
-} },
+        name: "QR Pro Scanner",
+        price: 409.99,
+        description: "Escáner de código de barras de escritorio, código QR, certificado Digital europeo, 2D",
+        image: "https://example.com/image_of_qr_pro_scanner.png",
+        video: "https://www.youtube.com/watch?v=xs-8jOXLKqE"
+    },
    {
         name: "QR Pocket Reader",
         price: 108.99,
         description: "NETUM-escáner de código de barras 2D QR de escritorio, lector de código de barras grande, USB, omnidireccional, manos libres A5, pantalla QR 1D",
-        image: "https://drive.google.com/file/d/1aXy3YUdzkpIX15KOIcrrujsEg8Pb6BVJ/view?usp=drive_link"
-    },
-    {
-        name: "QR Industrial Scanner",
-        price: 1399.99,
-        description: "Escáner robusto diseñado para entornos industriales con protección IP65.",
-        image: "https://m.media-amazon.com/images/I/51eU+Iy7b5L._AC_SX466_.jpg?height=200&width=200"
+        image: "https://drive.google.com/uc?export=view&id=1aXy3YUdzkpIX15KOIcrrujsEg8Pb6BVJ"
     },
     {
         name: "QR Industrial Scanner",
@@ -48,3 +42,4 @@ function addToCart(productName) {
 }
 
 document.addEventListener('DOMContentLoaded', renderProducts);
+
